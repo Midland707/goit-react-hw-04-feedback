48f753f3baa01bd9c91f31c90f94b883d5f1ca4d
Midland707 <rus14job@ukr.net>
2023-03-15 02:32:20 +0200

    refactoring to use Hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Section } from 'components/Section/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Statistics/Statistics';
 import { Notification } from 'components/Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    positivePercentage: 0,
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
+
+  const onLeaveFeedback = event => {
+    switch (event.target.textContent) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        alert(`Error! ${event.target.textContent} - not find. `);
+    }
   };
 
-  onLeaveFeedback = event => {
-    // console.log('textContent', event.target.textContent);
-    if (event.target.textContent === 'good')
-      this.setState(state => ({
-        good: state.good + 1,
-      }));
-    if (event.target.textContent === 'neutral')
-      this.setState(state => ({
-        neutral: state.neutral + 1,
-      }));
-    if (event.target.textContent === 'bad')
-      this.setState(state => ({
-        bad: state.bad + 1,
-      }));
+  useEffect(() => {
+    setTotal(good + neutral + bad);
+    setPositivePercentage(Number(((100 * good) / total).toFixed(0)));
+  }, [good, neutral, bad, total]);
 
-    this.countTotalFeedback();
-  };
-
-  countTotalFeedback() {
-    this.setState(state => ({
-      total: state.good + state.neutral + state.bad,
-    }));
-    this.countPositiveFeedbackPercentage();
-  }
-
-  countPositiveFeedbackPercentage = () => {
-    this.setState(state => ({
-      positivePercentage: Number(((100 * state.good) / state.total).toFixed(0)),
-    }));
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-          fontSize: 40,
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.state.total}
-            positivePercentage={this.state.positivePercentage}
-          />
-          <Notification
-            message="There is no feedback"
-            total={this.state.total}
-          />
-        </Section>
-      </div>
-    );
-  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        fontSize: 40,
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />
+        <Notification message="There is no feedback" total={total} />
+      </Section>
+    </div>
+  );
 }
